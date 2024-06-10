c27e2d820f44b5dc663cd1d93d29061cd30d29ed
Amirhossein Zarei <amir.zarei1381913@gmail.com>
2024-06-11 00:43:12 +0330

    Fix validation text

--- src/schema/TransactionValidation.jsx
+++ src/schema/TransactionValidation.jsx
@@ -1,7 +1,7 @@
 import * as yup from 'yup';
 
 export const TransactionValidation = yup.object().shape({
-    amount: yup.string().required('نام تگ الزامی است.'),
+    amount: yup.string().required('مقدار تگ الزامی است.'),
     description: yup.string().required('توضیحات تراکنش الزامی است.'),
     type: yup.string().required('نوع تراکنش الزامی است.'),
 });
